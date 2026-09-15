516dabf151cd963c0db6ce648349a45c4fcfd1cb
soham-bhide <sohambhide1603@gmail.com>
2026-09-15 23:57:46 +0530

    Added more endpoints

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -128,3 +128,89 @@ app.post("/organizations", middleware, async (req, res) => {
   }
 });
 
+app.get("/organizations",middleware,async(req,res)=>{
+try{
+  const members = await prisma.membership.findMany({
+    where:{
+      userId:req.userId
+    },
+    select:{
+      organization:true,
+      role:true,
+      joinedAt:true
+    }
+  })
+const formattedOrganizations = members.map((m) => ({
+      ...m.organization,
+      role: m.role,
+      joinedAt: m.joinedAt,
+    }));
+    return res.json({
+      formattedOrganizations
+    })
+
+}
+catch(e){
+  console.log(e);
+  return res.json({
+    message:"Something went wrong"
+  })
+}
+
+} )
+
+app.get("/organizations/:id", middleware, async (req, res) => {
+  try {
+    const orgid = req.params.id;
+
+    
+    const organizationcheck = await prisma.membership.findFirst({
+      where: {
+        userId: req.userId,
+        organizationId: orgid
+      }
+    });
+
+    if (!organizationcheck) {
+      return res.status(403).json({
+        message: "Access denied or organization not found"
+      });
+    }
+
+    
+    const members = await prisma.membership.findMany({
+      where: {
+        organizationId: orgid
+      },
+      select: {
+        role: true,
+        joinedAt: true,
+        user: {
+          select: {
+            id: true,
+            username: true,
+            email: true
+          }
+        }
+      }
+    });
+
+    
+    const formattedOrganizations = members.map((m) => ({
+      userId: m.user.id,
+      username: m.user.username,
+      email: m.user.email,
+      role: m.role,
+      joinedAt: m.joinedAt
+    }));
+
+    return res.json({
+      members: formattedOrganizations
+    });
+  } catch (e) {
+    console.log(e);
+    return res.json({
+      message: "Something went wrong"
+    });
+  }
+});
